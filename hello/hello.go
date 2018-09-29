a5fd2b9bb1c6253da78ade7e34747900818d7df5
Rojesh Shrestha <rojace2011@gmail.com>
2018-09-29 22:48:24 +0545

    Calling the package.

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/user/goplay/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.Abbreviate("World Health Organization of Lang"))
+}
