843d243c26f59843dd9e4e8e42a85acdfce08384
Rojesh Shrestha <rojace2011@gmail.com>
2018-09-29 23:07:30 +0545

    Taking input from console.

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/user/goplay/stringutil"
+	"os"
+	"bufio"
+	"strings"
 )
 
+const inputdelimiter = '\n'
+
 func main() {
-	fmt.Println(stringutil.Reverse("!oG ,olleH"))
-	fmt.Println(stringutil.Abbreviate("World Health Organization of Lang"))
+	fmt.Println("Enter the sentence: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString(inputdelimiter)
+	input = strings.Replace(input, "\n", "", -1)
+
+	fmt.Println(stringutil.Abbreviate(input))
 }
