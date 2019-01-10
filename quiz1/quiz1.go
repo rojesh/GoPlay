de8ce74161f19747136a4b4c479edb1f81b873b8
Rojesh Shrestha <rojace2011@gmail.com>
2019-01-10 14:28:26 +0545

    Indentation.

--- quiz1/quiz1.go
+++ quiz1/quiz1.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "encoding/csv"
-  "os"
-  "bufio"
-  "io"
-  "strings"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 const inputdelimiter = '\n'
@@ -14,33 +14,32 @@ const inputdelimiter = '\n'
 var count int
 
 func main() {
-  fmt.Printf("LETS TAKE ON THE QUIZ\n")
-  fmt.Printf("---------------------\n")
-  fmt.Printf("---------------------\n")
-  ReadCsvFile("quiz1.csv")
+	fmt.Printf("LETS TAKE ON THE QUIZ\n")
+	fmt.Printf("---------------------\n")
+	fmt.Printf("---------------------\n")
+	ReadCsvFile("quiz1.csv")
 }
 
-
-func ReadCsvFile(filePath string)  {
-    csvFile, _ := os.Open(filePath)
-    csvReader := csv.NewReader(bufio.NewReader(csvFile))
-    for {
-        record, err := csvReader.Read()
-        if err == io.EOF {
-          fmt.Printf("You got %d answers correct\n", count)
-          break
-        }
-        for index, value := range record {
-          if index == 0 {
-            fmt.Printf("What is %v\n", value)
-          } else {
-            reader := bufio.NewReader(os.Stdin)
-            input, _ := reader.ReadString(inputdelimiter)
-            input = strings.Replace(input, "\n", "", -1)
-            if input == value {
-              count++
-            }
-          }
-        }
-    }
+func ReadCsvFile(filePath string) {
+	csvFile, _ := os.Open(filePath)
+	csvReader := csv.NewReader(bufio.NewReader(csvFile))
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			fmt.Printf("You got %d answers correct\n", count)
+			break
+		}
+		for index, value := range record {
+			if index == 0 {
+				fmt.Printf("What is %v\n", value)
+			} else {
+				reader := bufio.NewReader(os.Stdin)
+				input, _ := reader.ReadString(inputdelimiter)
+				input = strings.Replace(input, "\n", "", -1)
+				if input == value {
+					count++
+				}
+			}
+		}
+	}
 }
